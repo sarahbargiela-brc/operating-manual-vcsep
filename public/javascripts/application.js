0f6d1f0cf1518228807e3101bebae83321a841b9
Harry Trimble <harryroberttrimble@gmail.com>
2020-03-28 15:26:53 +0000

    Fix step-by-step page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,21 +9,6 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-// Current date for when people print web page
-$(document).ready(function(){
-
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
-
-  months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
-  today = dd + ' ' + months[mm-1] + ' ' + yyyy;
-  console.log(today);
-  $('#currentdate').html(today);
-});
-
 // Copy test
 var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
 copyEmailBtn.addEventListener('click', function(event) {  
